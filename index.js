4350388f108b50ce9d8120e9ac8b5a1867345d7a
Mihai Bucse <mikitu78@gmail.com>
2017-11-29 12:40:05 +0000

    cleanup

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
 const osu = require('node-os-utils')
 const cpu = osu.cpu
-const pretty = require('prettysize');
-
-let drive = osu.drive
-let osCmd = osu.osCmd
-
-// osCmd.diskUsage().then(info => {
-//     console.log("df: ", info)
-// })
-let cp = require('child_process')
-
-cp.exec('df -kPh', { shell: true }, function (err, stdout, stderr) {
-    if (err || !stdout) {
-        console.error("err: ", err)
-    }
-
-    // console.log("Disks: ", parseDfStdout(stdout))
-})
-
-let mem = osu.mem
-
-let openfiles = osu.openfiles;
-
-let DISK_PATTERN = /^(\S+)\n?\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.+?)\n/mg
-
-function createDiskInfo (headlineArgs, args) {
-    let info = {}
-
-    headlineArgs.forEach(function (h, i) {
-        info[h] = args[i]
-    })
-
-    return info
-}
-
-function parseDfStdout (stdout) {
-    let dfInfo = []
-    let headline
-
-    stdout.replace(DISK_PATTERN, function () {
-        let args = Array.prototype.slice.call(arguments, 1, 7)
-
-        if (arguments[7] === 0) {
-            headline = args
-            return
-        }
-        dfInfo.push(createDiskInfo(headline, args))
-    })
-
-    return dfInfo
-}
-
 const express = require('express')
+const pretty = require('prettysize');
+const drive = osu.drive
+const osCmd = osu.osCmd
+const mem = osu.mem
+const openfiles = osu.openfiles;
 const app = express()
 const wrap = require('co-express');
+
 app.get('/', wrap(function* (req, res) {
     let cpu_usage = yield cpu.usage();
     let drive_info = yield drive.info();
@@ -74,9 +29,5 @@ app.get('/', wrap(function* (req, res) {
 
     })
 }))
-// app.use(function *(){
-//     let cpu_usage = yield cpu.usage();
-//     console.log(cpu_usage)
-//     next();
-// });
-app.listen(3001, () => console.log('Example app listening on port 3000!'))
+let port = process.env.PORT || 8087;
+app.listen(port, () => console.log('App listening on port ' + port))
